363420b7cd03f519b0fd06b2f0120e5f636867b7
Azfar <s.azfar.rahman@gmail.com>
2024-02-28 21:32:07 +0400

    Experimented with quantity getters and setters

--- project.py
+++ project.py
@@ -2,25 +2,36 @@ class Stock():
 
     money = 10000
 
-    def __init__(self, name: str, quantity: int)
+    def __init__(self, name: str, quantity: int):
         self._name = name
-        self._quantity = quantity
-    
+        self._quantity = 0
+        self.quantity("buy", quantity)
+
     @property
-    def stock_name(self):
+    def name(self):
         return self._name
     
-    @stock_name.setter
-    def change_stock(self, type: str):
-        if type.lower() != "buy" and type != "sell":
-            print("Error, expected buy or sell as argument")
+    @property
+    def quantity(self):
+        return self._quantity
 
+    @quantity.setter
+    def quantity(self, type: str, quantity: int):
+        type = type.lower()
 
+        if type != "buy" and type != "sell":
+            print("Error, expected buy or sell")
+
+        if type == "buy":
+            self._quantity += quantity
+
+    def __str__(self):
+        return f"{self._name}, Quantity: {self._quantity}"
 
 
 
 def main():
-
-
+    APPL = Stock("APPL", 10)
+    print(APPL)
 if __name__ == "__main__":
     main()
