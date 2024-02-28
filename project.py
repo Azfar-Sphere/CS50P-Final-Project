f07573f8ab86916f69a193b8e645acc27ef713cd
Azfar <s.azfar.rahman@gmail.com>
2024-02-28 22:49:12 +0400

    added money class method

--- project.py
+++ project.py
@@ -1,17 +1,17 @@
 class Stock():
 
-    money = 10000
+    money_ = 10000
 
     def __init__(self, name: str):
         self._name = name
         self._quantity = 0
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self._name
     
     @property
-    def quantity(self):
+    def quantity(self) -> int:
         return self._quantity
 
     def buy(self, quantity: int):
@@ -20,17 +20,21 @@ class Stock():
     def sell(self, quantity: int):
         self._quantity -= quantity
 
-    def price(self):
+    def price(self) -> int:
         self._name
     
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self._name}, Quantity: {self._quantity}"
 
-
+    @classmethod
+    def money(cls) -> int:
+        return cls.money_
 
 def main():
     AAPL = Stock("AAPL")
     AAPL.buy(10)
     print(AAPL)
+    print(Stock.money())
+
 if __name__ == "__main__":
     main()
