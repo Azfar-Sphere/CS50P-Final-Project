79de68742cc48fbcff80767481b12ef5395d99d0
Azfar <s.azfar.rahman@gmail.com>
2024-02-28 21:08:21 +0400

    Added getters and setters and money as class property

--- project.py
+++ project.py
@@ -1,5 +1,25 @@
+class Stock():
+
+    money = 10000
+
+    def __init__(self, name: str, quantity: int)
+        self._name = name
+        self._quantity = quantity
+    
+    @property
+    def stock_name(self):
+        return self._name
+    
+    @stock_name.setter
+    def change_stock(self, type: str):
+        if type.lower() != "buy" and type != "sell":
+            print("Error, expected buy or sell as argument")
+
+
+
+
+
 def main():
-    print("This is my CS50p Final Project")
 
 
 if __name__ == "__main__":
