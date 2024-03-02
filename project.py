91596a32dd42b124bc37b0d985f386eae0aae3d0
Azfar <s.azfar.rahman@gmail.com>
2024-03-02 23:18:52 +0400

    added completetion marking and notes

--- project.py
+++ project.py
@@ -1,11 +1,24 @@
+from datetime import datetime
+
 class Habit():
 
-    def __init__(self, name, duration, frequency) -> None:
-        self._name = name
-        self._duration = duration
-        self._frequency = frequency
+    def __init__(self, name, frequency, start_date=None) -> None:
+        self.name = name
+        self.frequency = frequency
+        self.streak = 0
+        self.start_date = start_date or datetime.date.today()
+        self.completed_today = False
+        self.notes = []
 
+    def mark_completed(self) -> None:
+        self.compeleted_today = True
+        self.streak += 1
 
+    def mark_uncompleted(self) -> None:
+        self.compeleted_today = False
+
+    def add_note(self, note: str) -> None:
+        self.notes.append(note)
 
 def main():
 
