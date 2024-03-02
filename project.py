6b46073613f8140f1d3e2bd82cde66e87b4e3bbf
Azfar <s.azfar.rahman@gmail.com>
2024-03-02 23:10:41 +0400

    added habit class

--- project.py
+++ project.py
@@ -1,40 +1,13 @@
-class Stock():
+class Habit():
 
-    money_ = 10000
-
-    def __init__(self, name: str):
+    def __init__(self, name, duration, frequency) -> None:
         self._name = name
-        self._quantity = 0
+        self._duration = duration
+        self._frequency = frequency
 
-    @property
-    def name(self) -> str:
-        return self._name
-    
-    @property
-    def quantity(self) -> int:
-        return self._quantity
 
-    def buy(self, quantity: int):
-        self._quantity += quantity
-
-    def sell(self, quantity: int):
-        self._quantity -= quantity
-
-    def price(self) -> int:
-        self._name
-    
-    def __str__(self) -> str:
-        return f"{self._name}, Quantity: {self._quantity}"
-
-    @classmethod
-    def money(cls) -> int:
-        return cls.money_
 
 def main():
-    AAPL = Stock("AAPL")
-    AAPL.buy(10)
-    print(AAPL)
-    print(Stock.money())
 
 if __name__ == "__main__":
     main()
