f2d7bc60c6ade328b1604bfde52028dc2c7675b5
Azfar <s.azfar.rahman@gmail.com>
2024-03-04 15:22:57 +0400

    gui creates customtkinter constrcutor

--- project.py
+++ project.py
@@ -27,13 +27,13 @@ class Habit():
 
 
 def main():
-    customtkinter.set_appearance_mode("System")
+    customtkinter.set_appearance_mode("dark")
     customtkinter.set_default_color_theme("blue")
     
     create_gui()
 
 def create_gui() -> None:
-    gui = tkinter.Tk()
+    gui = customtkinter.CTk()
     gui.title("Habit Tracker")
     gui.geometry("800x600")
 
