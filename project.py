d902c54031216a28374d8e2eb1653ae9afa73711
Azfar <s.azfar.rahman@gmail.com>
2024-03-11 12:31:57 +0400

    fixed global variables and button column

--- project.py
+++ project.py
@@ -65,7 +65,8 @@ def create_gui() -> None:
 
     z = 2
 
-    for i, object in enumerate(habits_objects_list):  
+    for i, object in enumerate(habits_objects_list):
+
         if today in object.days:
             object_name = object.name
             habit_name_label = ttk.Label(frame, text=object_name, style='TLabel')
@@ -77,24 +78,27 @@ def create_gui() -> None:
             z += 1
 
     button = ttk.Button(frame, text="Add Habit", command=create_habit, style='TButton')
-    button.grid(column=0, row=z+1, columnspan=2, pady=10)
+    button.grid(column=0, row=z+2, columnspan=2, pady=10)
 
+    global habit_name
     habit_name = StringVar()
 
     label1 = ttk.Label(frame, text="Name", padding=10, style='TLabel')
-    label1.grid(column=0, row=z+2, columnspan=2)
+    label1.grid(column=0, row=z+3, columnspan=2)
 
     habit_entry = ttk.Entry(frame, textvariable=habit_name)
-    habit_entry.grid(column=0, row=z+3, columnspan=2, pady=5)
+    habit_entry.grid(column=0, row=z+4, columnspan=2, pady=5)
 
+    global days_list
     days_list = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     daysvar = StringVar(value=days_list)
 
     label2 = ttk.Label(frame, text="Days", padding=10, style='TLabel')
-    label2.grid(column=0, row=z+4, columnspan=2)
+    label2.grid(column=0, row=z+5, columnspan=2)
 
+    global days_listbox
     days_listbox = Listbox(frame, listvariable=daysvar, height=7, selectmode="extended")
-    days_listbox.grid(column=0, row=z+5, columnspan=2, pady=5)
+    days_listbox.grid(column=0, row=z+6, columnspan=2, pady=5)
 
     for i in range(len(days_list) + 7):
         frame.rowconfigure(i, weight=1)
